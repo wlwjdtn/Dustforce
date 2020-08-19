18de936ab6215cac8ac168afb1492014d00edbca
wlwjdtn <wlwjdtn1592@gmail.com>
2020-08-19 11:11:35 +0900

    Platform_Move_Set

--- DustForce/Assets/Scripts/System/platform_Controller.cs
+++ DustForce/Assets/Scripts/System/platform_Controller.cs
@@ -46,49 +46,85 @@ public class platform_Controller: Raycast_Controller {
                 }
             }
         }
+        if (velocity.x != 0) {
+            float rayLength = Mathf.Abs(velocity.x) + _SkinWidth;
+            // 레이 캐스트 사용
+            for (int i = 0; i < _HoriRayCount; i++) {
+                Vector2 rayOrigin = (directionX == -1) ? _RayOrigins._BottomLeft : _RayOrigins._BottomRight;
+                rayOrigin += Vector2.up * (_HoriRaySpacing * i);
+                RaycastHit2D _Hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+
+                if (_Hit) {
+                    if (!movedPassengers.Contains(_Hit.transform)) {
+                        movedPassengers.Add(_Hit.transform);
+                        float pushX = velocity.x - (_Hit.distance - _SkinWidth) * directionX;
+                        float pushY = 0;
+                        _Hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+        if(directionY == -1 || velocity.y == 0 && velocity.x != 0) {
+            float rayLength = _SkinWidth * 2;
+
+            for (int i = 0; i < _VerRayCount; i++) {
+                Vector2 rayOrigin = _RayOrigins._TopLeft + Vector2.right * (_VerRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                if (hit) {
+                    if (!movedPassengers.Contains(hit.transform)) {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x;
+                        float pushY = velocity.y;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+
+        //public Vector3 move;
+        //public LayerMask passengerMask;
+
+        //public override void Start() {
+        //    base.Start();
+        //}
+
+        //private void Update() {
+        //    UpdateRayCastOrigins();
+
+        //    Vector3 velocity = move * Time.deltaTime;
+        //    MovePassengers(velocity);
+        //    transform.Translate(velocity);
+        //}
+
+        //private void MovePassengers(Vector3 velocity) {
+        //    HashSet<Transform> movedPassengers = new HashSet<Transform>();
+
+
+        //    float directionX = Mathf.Sign(velocity.x);
+        //    float directionY = Mathf.Sign(velocity.y);
+
+        //    if (velocity.y != 0) {
+        //        float rayLength = Mathf.Abs(velocity.y) + _SkinWidth;
+
+        //        // 레이 캐스트 사용
+        //        for (int i = 0; i < _VerRayCount; i++) {
+        //            Vector2 rayOrigin = (directionY == -1) ? _RayOrigins._BottomLeft : _RayOrigins._TopLeft;
+        //            rayOrigin += Vector2.right * (_VerRaySpacing * i);
+        //            RaycastHit2D _Hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+
+        //            if (_Hit) {
+        //                if (!movedPassengers.Contains(_Hit.transform)) {
+        //                    movedPassengers.Add(_Hit.transform);
+        //                    float pushX = (directionY == 1) ? velocity.x : 0;
+        //                    float pushY = velocity.y - (_Hit.distance - _SkinWidth) * directionY;
+
+        //                    _Hit.transform.Translate(new Vector3(pushX, pushY));
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
     }
-
-    //public Vector3 move;
-    //public LayerMask passengerMask;
-
-    //public override void Start() {
-    //    base.Start();
-    //}
-
-    //private void Update() {
-    //    UpdateRayCastOrigins();
-
-    //    Vector3 velocity = move * Time.deltaTime;
-    //    MovePassengers(velocity);
-    //    transform.Translate(velocity);
-    //}
-
-    //private void MovePassengers(Vector3 velocity) {
-    //    HashSet<Transform> movedPassengers = new HashSet<Transform>();
-
-
-    //    float directionX = Mathf.Sign(velocity.x);
-    //    float directionY = Mathf.Sign(velocity.y);
-
-    //    if (velocity.y != 0) {
-    //        float rayLength = Mathf.Abs(velocity.y) + _SkinWidth;
-
-    //        // 레이 캐스트 사용
-    //        for (int i = 0; i < _VerRayCount; i++) {
-    //            Vector2 rayOrigin = (directionY == -1) ? _RayOrigins._BottomLeft : _RayOrigins._TopLeft;
-    //            rayOrigin += Vector2.right * (_VerRaySpacing * i);
-    //            RaycastHit2D _Hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
-
-    //            if (_Hit) {
-    //                if (!movedPassengers.Contains(_Hit.transform)) {
-    //                    movedPassengers.Add(_Hit.transform);
-    //                    float pushX = (directionY == 1) ? velocity.x : 0;
-    //                    float pushY = velocity.y - (_Hit.distance - _SkinWidth) * directionY;
-
-    //                    _Hit.transform.Translate(new Vector3(pushX, pushY));
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
 }
